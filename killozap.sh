39fe03021fa53c3437620df35b05c52a72509ffd
Stuart Charlton <scharlton@pivotal.io>
2017-01-04 13:40:57 -0700

    Fixes ripley feature; adds cells as a narrow consul target.

--- killozap.sh
+++ killozap.sh
@@ -3,14 +3,15 @@
 # Presumes PCF job name patterns
 # Useful for quorum loss
 
-if [[ ($1 == "consul") || ($1 == "etcd" ) || ($1 == "bbs" ) || ($1 == "nuke") ]]
+if [[ ($1 == "consul") || ($1 == "etcd" ) || ($1 == "bbs" ) || ($1 == "ripley") || ($1 == "cells") ]]
         then
-                echo "Running CF $1 Process ..."
+                echo "Kill-o-Zapping your current CF deployment... "
         else
-                echo "Usage: $0 [consul|bbs|etcd|ripley]"
+                echo "Usage: $0 [consul ([all|)|bbs|etcd|ripley]"
                 echo "consul will stop all consul agent instances, delete /var/vcap/store/consul_agent/* recursively, and restart them"
                 echo "bbs will stop all diego bbs etcd processes, delete /var/vcap/store/etcd/* recursively, and restart them"
                 echo "etcd will stop all non-diego-bbs etcd job processes, delete /var/vcap/store/etcd/* recursively, and restart them"
+                echo "cells will stop all diego_cell consul_agent job processes, delete /var/vcap/store/consul_agent/* recursively, and restart them"
                 echo "ripley will nuke the site from orbit; aka all of the above"
                 exit 1
 fi
@@ -76,13 +77,19 @@ if [ $1 == "etcd" ]; then
  nukeProcesses etcd
 fi
 
+if [ $1 == "cells" ]; then
+ jobVMs=$(bosh instances --ps | awk -F "|" 'RS="\\+\\-\\-" {gsub(/ /, "", $0); for (i=2; i<= NF; i+=6) printf "%s\n", (i>2) ? $2 "," $i : "" }'| grep diego_cell)
+ nukeProcesses consul_agent
+fi
+
 if [ $1 == "bbs" ]; then
  jobVMs=$(bosh instances --ps | awk -F "|" 'RS="\\+\\-\\-" {gsub(/ /, "", $0); for (i=2; i<= NF; i+=6) printf "%s\n", (i>2) ? $2 "," $i : "" }'| grep diego_database)
  nukeProcesses etcd
 fi
+
 if [ $1 == "ripley" ]; then
+ jobVMs=$(bosh instances --ps | awk -F "|" 'RS="\\+\\-\\-" {gsub(/ /, "", $0); for (i=2; i<= NF; i+=6) printf "%s\n", (i>2) ? $2 "," $i : "" }')
  nukeProcesses consul_agent
- nukeProcesses bbs
  nukeProcesses etcd
 fi
 
